7ee8432d7a381ff1a94570c15d2d8fb6567d7e16
Ted Neward <ted@tedneward.com>
2011-01-22 14:44:17 -0800

    Initial import from Win7DotNet2010

---
+++ DallasTechFest/ConsoleApplication1/ConsoleApplication1/Program.fs
@@ -0,0 +1,32 @@
+﻿// Learn more about F# at http://fsharp.net
+open System
+
+let ints = [5; 4; 3; 2; 1; ]
+let countList = List.length
+
+let add l r = l + r
+let add5 r = (fun l r -> l + r) 5
+
+let message = "Howdy Dallas"
+System.Console.WriteLine(message.Substring(6))
+
+let x : int16 = 5s
+
+for i = 1 to 10 do
+    Console.WriteLine(i)
+
+let swap (a, b) = (b, a)
+
+type Person(firstName, lastName, age) =
+    member per.FirstName = firstName
+    member this.LastName = lastName
+    override p.ToString() =
+        System.String.Format("{0} {1} ({2} years old)", firstName, lastName, age)
+
+let s = new Person("Scott", "Davis", 102)
+Console.WriteLine(s.FirstName)
+
+let pl = [ new Person("Scott", "Davis", 65); new Person("Ted", "Neward", 40);
+            new Person("Tim", "Rayburn", 65); new Person("Craig", "Walls", 35) ]
+let sumAges = List.map (fun (p : Person) -> p.Age) |> List.collect (+)
+Console.WriteLine(sumAges)
---
+++ DevConLondon/ConsoleApplication1/ConsoleApplication1/Program.fs
@@ -0,0 +1,33 @@
+﻿// Learn more about F# at http://fsharp.net
+
+open System
+
+let message = "Howdy, London!"
+System.Console.WriteLine(message)
+
+for i = 1 to 10 do
+    System.Console.WriteLine("i = {0}", i)
+    if i % 2 = 0 then
+        System.Console.WriteLine("i^2 = {0}", i * i)
+
+type Person(firstName, lastName, age) =
+    member p.FirstName = firstName
+    member this.LastName = lastName
+    member Me.Age = age
+    override p.ToString() = 
+        String.Format("[Person: {0} {1} {2}", p.FirstName, lastName, p.Age)
+
+type Student(firstName, lastName, age, subject) =
+    inherit Person(firstName, lastName, age)
+    member s.Subject = subject
+    member s.DrinkBeer(?kind : string, ?amount : int) =
+        let bk = match kind with None -> "Keystone" | Some name -> name
+        let amnt = match amount with None -> 6 | Some amt -> amt
+        Console.WriteLine("I'm drinking {0} {1}s", amnt, bk)
+
+let t = new Student("Ted", "Neward", 39, "Girls")
+Console.WriteLine("t's firstName is {0}", t.FirstName)
+Console.WriteLine("t = {0}", t)
+let t2 = new Person(t.FirstName, t.LastName, t.Age + 1)
+Console.WriteLine("t's age is now {0}", t2.Age)
+t.DrinkBeer(amount=12, kind="Samuel Adams")
