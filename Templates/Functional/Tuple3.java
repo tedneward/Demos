364745808a42f1a66142153f0e76d104c8945e1f
Ted Neward <ted@tedneward.com>
2016-08-12 00:42:50 -0700

    Rearranging some VSLive demos

--- Templates/Functional/Tuple3.java
+++ Templates/Functional/Tuple3.java
@@ -1,29 +1,27 @@
-public class Tuple3<T1,T2,T3>
+public class Tuple2<T1,T2>
     implements java.io.Serializable
 {
-    public Tuple3(T1 t1, T2 t2, T3 t3)
+    public Tuple2(T1 t1, T2 t2)
     {
-        _1 = t1; _2 = t2; _3 = t3;
+        _1 = t1; _2 = t2;
     }
-    
+ 
     @SuppressWarnings("unchecked")
     public boolean equals(Object other)
     {
         if (other == this)
             return true;
         
-        if (other instanceof Tuple3)
+        if (other instanceof Tuple2)
         {
             // This cast generates the "unchecked" warning
-            Tuple3<T1,T2,T3> rhs = (Tuple3<T1,T2,T3>)other;
+            Tuple2<T1,T2> rhs = (Tuple2<T1,T2>)other;
             
             return (
                 (this._1.getClass().isAssignableFrom(rhs._1.getClass())) &&
                 (this._2.getClass().isAssignableFrom(rhs._2.getClass())) &&
-                (this._3.getClass().isAssignableFrom(rhs._3.getClass())) &&
                 (this._1.equals(rhs._1)) &&
-                (this._2.equals(rhs._2)) &&
-                (this._3.equals(rhs._3))
+                (this._2.equals(rhs._2))
             );
         }
         
@@ -32,21 +30,16 @@ public class Tuple3<T1,T2,T3>
     
     public int hashCode()
     {
-        return _1.hashCode() & _2.hashCode() & _3.hashCode();
+        return _1.hashCode() & _2.hashCode();
     }
     
     public String toString()
     {
-        return "Tuple3<" + 
-            _1.getClass().toString() + "," +
-            _2.getClass().toString() + "," +
-            _3.getClass().toString() + ">" +
-            "(" + _1.toString() + "," +
-            _2.toString() + "," +
-            _3.toString() + ")";
+        return "Tuple2<>(" + 
+            _1.toString() + "," +
+            _2.toString() + ")";
     }
     
     public T1 _1;
     public T2 _2;
-    public T3 _3;
 }
